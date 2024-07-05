6becd7e6668e01784c8ae3a017f4ef4428268b57
kennie-larkson <materialkenny@gmail.com>
2024-07-05 15:43:54 +0100

    removed build script from vercel.json

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 app.set("trust proxy", true);
 
