a90bc86ce021f8daf8828056bac498e2b044f093
kennie-larkson <materialkenny@gmail.com>
2024-07-06 14:10:46 +0100

    edited vercel.json build object

---
+++ dist/routes/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRouter = void 0;
+var userRouter_1 = require("./userRouter");
+Object.defineProperty(exports, "userRouter", { enumerable: true, get: function () { return userRouter_1.userRouter; } });
